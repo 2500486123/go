003fbe7925f00eef7420075e138bb1db246eaf49
liliangpeng <2500486123llp>
2021-07-22 00:19:13 +0800

    *

--- 2.基本结构/hello.go
+++ 2.基本结构/hello.go
@@ -1,8 +1,24 @@
+// 定义包名
 package main
 
+//引入fmt包
 import "fmt"
 
+//声明函数
+//main 函数是每一个可执行程序所必须包含的，一般来说都是在启动后第一个执行的函数（如果有 init() 函数则会先执行该函数）
 func main() {
-	/*这是我的第一个简单程序*/
-	fmt.Println("Hello, 小姐姐!")
+	//声明变量
+	var hello = "Hello, 小姐姐!"
+
+	//语句 可以将字符串输出到控制台
+	fmt.Println(hello)
 }
+
+//单行注释
+/*
+多
+行
+注
+释
+以上就是go语言的基本结构
+*/
